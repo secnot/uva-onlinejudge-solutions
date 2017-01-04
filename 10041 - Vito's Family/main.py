7f4b78483f05deff2800e35b463da877c46297b8
secnot <secnot@secnot.com>
2017-01-04 01:38:29 +0100

    Solved 10041 - Vito's Family

--- 10041 - Vito's Family/main.py
+++ 10041 - Vito's Family/main.py
@@ -1,13 +1,30 @@
 import sys
 
 
+def read_num():
+    line = sys.stdin.readline()
+    return list(map(int, line.rstrip().split()))
 
 
+def sum_distances(house, relatives):
+    return sum(abs(r-house) for r in relatives)
 
 
-
-
+def optimal_house(relatives, numbers):
+    s = sorted(numbers)
+    return s[len(s)//2]
 
 
 if __name__ == '__main__':
+    
+    tests = read_num()[0]
+
+    for i in range(tests):
+        test = read_num()
+        relatives = test[0]
+        numbers = test[1:]
+
+        house = optimal_house(relatives, numbers)
+        print(sum_distances(house, numbers))
+    
     exit(0)

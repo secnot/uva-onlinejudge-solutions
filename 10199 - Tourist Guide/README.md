5b3a0c9b36d0772cd4f634f37112e7389681120d
secnot <secnot@secnot.com>
2017-01-15 11:57:49 +0100

    First try for 10199 - Tourist Guide

---
+++ 10199 - Tourist Guide/README.md
@@ -0,0 +1,92 @@
+# 10199 - Tourist Guide
+
+Rio de Janeiro is a very beautiful city. But there are so many places to visit that sometimes you feel
+a bit lost. But don’t worry, because your friend Bruno promised you to be your tourist guide. The
+problem is that he is not a very good driver, as he can’t see very well (poor Bruno).
+
+Because of his disabilities, Bruno have a lot of fines to pay and he doesn’t want to have even more
+to pay, even though he promised you to be your tourist guide. What he would like to know, however,
+is where are all the cameras that help the police to fine the bad drivers, so he can drive more carefully
+when passing by them.
+
+Those cameras are strategically distributed over the city, in locations that a driver must pass through
+in order to go from one zone of the city to another. In order words, if there are two city locations A
+and B such that to go from one to another (A to B or B to A) a driver must pass through a location
+C, then C will have a camera.
+
+For instance, suppose that we have 6 locations (A, B, C, D, E and F) and that we have 7 routes
+(all of them bidirectonal) B-C, A-B, C-A, D-C, D-E, E-F and F-C. There’s a camera on C because to
+go from A to E, for instance, you must pass through C. In this configuration, there’s only one camera
+(on C).
+
+Your task is to help Bruno (as he wants to be a musician and he doesn’t want to get even close of
+computers) and write a program which will tell him where are all the cameras, given the map of the
+city, so he can be your tourist guide and avoid further fines.
+
+
+## Input
+
+The input will consist on an arbitrary number of city maps. Each city map will begin with an integer
+*N* (2 < *N* ≤ 100) which stands for the total locations of the city. Then will follow *N* different place
+names, one per line. Each place name will have at least one and at most 30 characters (all of them will
+be lowercase latin letters). Then will follow a non-negative integer *R* which stands for the total routes
+of the city. Then will follow R lines with the routes. Each route will be represented by the name of
+both places that the route connects (remember that the routes are bidirectional). Location names in
+route description will always be valid and there will be no route from one place to itself. You must
+read until *N* = 0, and this input should not be processed.
+
+
+## Output
+
+For each city map you must print the line:
+
+```bash
+City map #d: c camera(s) found
+```
+
+Where d stands for the city map number (starting from 1) and c stands for the total number of
+cameras. Then should follow *c* lines with the location names where are each camera (in alphabetic
+order). You should print a blank line between output sets.
+
+
+## Sample Input
+
+```bash
+6
+sugarloaf
+maracana
+copacabana
+ipanema
+corcovado
+lapa
+7
+ipanema copacabana
+copacabana sugarloaf
+ipanema sugarloaf
+maracana lapa
+sugarloaf maracana
+corcovado sugarloaf
+lapa corcovado
+5
+guanabarabay
+downtown
+botanicgarden
+colombo
+sambodromo
+4
+guanabarabay sambodromo
+downtown sambodromo
+sambodromo botanicgarden
+colombo sambodromo
+0
+```
+
+
+## Sample Output
+
+```bash
+City map #1: 1 camera(s) found
+sugarloaf
+City map #2: 1 camera(s) found
+sambodromo
+```
---
+++ 10199 - Tourist Guide/main.py
@@ -0,0 +1,293 @@
+import sys
+from itertools import count
+from collections import defaultdict, deque
+
+
+
+class Graph(object):
+
+    def __init__(self, edges=None, directed=False):
+        self._adj = defaultdict(set)
+        self._directed = directed
+        if edges:
+            self.add_edges(edges)
+ 
+        self._process_vertex_early = self._default_process_vertex_early
+        self._process_vertex_late = self._default_process_vertex_late
+        self._process_edge = self._default_process_edge
+  
+    def vertices(self):
+        return list(self._adj.keys())
+
+    def edges(self):
+        if self._directed:
+            return [(ver, e) for ver, edg in self._adj.items() for e in edg]
+        else:
+            edges = set()
+            for ver, edg in self._adj.items():
+                for e in edg:
+                    edges.add((max(ver, e), min(ver, e)))
+            return list(edges)
+
+    def add_edges(self, edges):
+        for v1, v2 in edges:
+            self.add_edge(v1, v2)
+
+    def add_edge(self, n1, n2):
+        """Add edge between n1 and n2 (also adding vertices)"""
+        self._adj[n1].add(n2)
+        if not self._directed:
+            self._adj[n2].add(n1)
+
+    def del_edge(self, n1, n2):
+        """ """
+        self._adj[n1].remove(n2)
+        if not self._directed:
+            self._adj[n2].remove(n1)
+
+    def add_vertex(self, v):
+        if v not in self._adj:
+            self._adj[v] = set()
+
+    def del_vertex(self, v):
+        """Delete vertex and all edges leading to it"""
+        if self._directed:
+            for _, edges in self._adj.items():
+                if v in edges:
+                    edges.remove(v)
+        else:
+            for e in self._adj[v]:
+                self._adj[e].remove(v)
+
+        del self._adj[v]
+
+    def vertex_connected(self, v1, v2):
+        """is v1 directly connected to v2"""
+        return v1 in self._adj and v2 in self._adj[v1]
+
+    def is_connected(self, v=None):
+        """True if graph if fully connected """
+        if v is None:
+            v = next(iter(self._adj.keys()))
+
+        self._process_vertex_early = self._default_process_vertex_early
+        self._process_vertex_late = self._default_process_vertex_late
+        self._process_edge = self._default_process_edge
+       
+        self.bfs(v)
+
+        parents = (value for key, value in self._parent.items() if key!=v)
+        return all((p is not None) for p in parents)
+
+    @staticmethod
+    def _default_process_vertex_early(self, v):
+        return
+
+    @staticmethod
+    def _default_process_vertex_late(self, v):
+        return
+
+    @staticmethod
+    def _default_process_edge(self, v, e):
+        return
+
+    def bfs(self, start):
+        """Breath first search implementation"""
+        self._discovered = defaultdict(bool)
+        self._processed = defaultdict(bool)
+        self._parent = defaultdict(lambda: None)
+
+        q = deque([start])
+        self._discovered[start] = True
+        while q:
+            v = q.popleft()
+            self._process_vertex_early(self, v)
+            self._processed[v] = True
+
+            for e in self._adj[v]:
+                if not self._processed[e] or self._directed:
+                    self._process_edge(self, v, e)
+                if not self._discovered[e]:
+                    q.append(e)
+                    self._discovered[e] = True
+                    self._parent[e] = v
+
+            self._process_vertex_late(self, v)
+
+    def dfs(self, v):
+        """Depth first search (recursive)"""   
+        self._discovered = defaultdict(bool)
+        self._processed = defaultdict(bool)
+        self._entry_time = defaultdict(lambda: None) # Entry time
+        self._exit_time = defaultdict(lambda: None) # Exit time
+        self._parent = defaultdict(lambda: None)
+        self._finished = False
+        self._time = count()
+        
+        return self._dfs(v)
+
+    def _dfs(self, v):
+
+        if self._finished:
+            return
+
+        self._discovered[v] = True
+        self._entry_time[v] = next(self._time)
+
+        self._process_vertex_early(self, v)
+
+        for e in self._adj[v]:
+            if not self._discovered[e]:
+                self._parent[e] = v
+                self._process_edge(self, v, e)
+                self._dfs(e)
+            elif not self._processed[e] and self._parent[v]!=e or self._directed:
+                self._process_edge(self, v, e)
+
+            if self._finished:
+                return
+
+        self._process_vertex_late(self, v)
+        self._exit_time[v] = next(self._time)
+        self._processed[v] = True
+
+    def find_path(self, n1, n2):
+
+        if len(self._adj) < 2:
+            return None
+
+        self._process_vertex_early = self._default_process_vertex_early
+        self._process_vertex_late = self._default_process_vertex_late
+        self._process_edge = self._default_process_edge
+        if n1 not in self._adj or n2 not in self._adj:
+            raise KeyError
+        self.bfs(n1)
+        if self._parent[n2] is None:
+            return None
+
+        # Reconstruct path
+        path = [n2]
+        current = n2
+        while self._parent[current] is not None:
+            path.append(self._parent[current])
+            current = self._parent[current]
+
+        return list(reversed(path))
+
+    def __str__(self):
+        return '{}({})'.format(self.__class__.__name__, dict(self._adj))
+
+    def __repr__(self):
+        return '{}({}, directed={})'.format(self.__class__.__name__, self.edges(), self._directed)
+
+
+def read_num():
+    return int(sys.stdin.readline())
+
+def load_locations(nlocations):
+    locations = [sys.stdin.readline().rstrip() for _ in range(nlocations)]
+    return {loc:num for num, loc in enumerate(locations)}
+
+def load_routes(nroutes, loc_table): 
+    routes = [sys.stdin.readline().rstrip().split() for _ in range(nroutes)]
+    return [(loc_table[start], loc_table[end]) for start, end in routes]
+
+def load_map():
+    nlocations = read_num()
+    if nlocations == 0:
+        return None, None
+    locations = load_locations(nlocations)
+
+    #
+    routes = load_routes(read_num(), locations)    
+    g = GraphAV()
+    g.add_edges(routes)
+    return g, {num: loc for loc, num in locations.items()}
+
+def edge_class():
+    pass
+
+
+
+class GraphAV(Graph):
+    """Graph articulation vertices"""
+    
+    @staticmethod
+    def _av_process_vertex_early(self, v):
+        self._reachable_ancestor[v] = v
+
+    @staticmethod
+    def _av_process_vertex_late(self, v):
+        # Is a root node 
+        if self._parent[v] is None:
+            if self._tree_out_degree[v] > 1:
+                self._articulation_points.add(v)
+            return
+
+        # Test if parent is root
+        parent_root = self._parent[self._parent[v]] is None
+
+        if not parent_root:
+            if self._reachable_ancestor[v] == self._parent[v]:
+                self._articulation_points.add(self._parent[v])
+
+            if self._reachable_ancestor[v] == v:
+                self._articulation_points.add(self._parent[v])
+
+                # Check it is not a leaf
+                if self._tree_out_degree[v] > 0:
+                    self._articulation_points.add(v)
+
+        time_v = self._entry_time[self._reachable_ancestor[v]]
+        time_parent = self._entry_time[self._reachable_ancestor[self._parent[v]]]
+
+        if time_v < time_parent:
+            self._reachable_ancestor[self._parent[v]] = self._reachable_ancestor[v]
+
+    @staticmethod
+    def _av_process_edge(self, v, e):
+        
+        edge_class = self._av_edge_class(v, e)
+
+        if edge_class == "tree":
+            self._tree_out_degree[v] += 1
+
+        if edge_class == "back" and self._parent[v] != e:
+            if self._entry_time[e] < self._entry_time[self._reachable_ancestor[v]]:
+                self._reachable_ancestor[v] = e
+
+    def _av_edge_class(self, v, e):
+        if self._parent[e] == v: return "tree"
+        if self._discovered[e] and not self._processed[e]: return "back"
+        if self._processed[e] and (self._entry_time[e]>self._entry_time[v]): return "forward"
+        if self._processed[e] and (self._entry_time[e]<self._entry_time[v]): return "cross"
+
+    def find_articulation(self):
+        """Find articulation points"""
+        if len(self._adj) < 3 or not self.is_connected():
+            return []
+
+        self._process_vertex_early = self._av_process_vertex_early
+        self._process_vertex_late = self._av_process_vertex_late
+        self._process_edge = self._av_process_edge
+        self._reachable_ancestor = defaultdict(lambda: None)
+        self._tree_out_degree = defaultdict(int)
+        self._articulation_points = set()
+
+        start = next(iter(self._adj.keys()))
+        self.dfs(start)
+        
+        return list(self._articulation_points)
+
+if  __name__ == '__main__':
+
+
+    for n in range(1, 9999999):
+        graph, locations = load_map()
+        if not graph:
+            break
+        
+        art = graph.find_articulation()
+        print('City map #{}: {} camera(s) found'.format(n, len(art)))
+        [print(a) for a in sorted(locations[a] for a in art)]
+        print('')

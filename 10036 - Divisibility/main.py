d7b67c6b6501b982ca7f8014d78471ca2bad02ea
secnot <secnot@secnot.com>
2017-01-18 21:50:00 +0100

    Code cleanup

--- 10036 - Divisibility/main.py
+++ 10036 - Divisibility/main.py
@@ -1,15 +1,12 @@
 import sys
 
-
 def load_num():
     return list(map(int, sys.stdin.readline().split()))
-
 
 def load_case():
     _, divisor = load_num()
     numbers = load_num()
     return numbers, divisor
-
 
 def is_divisible(num, k):
     n = len(num)
@@ -25,12 +22,8 @@ def is_divisible(num, k):
 
     return rem[n-1][0]
 
-
-if __name__ == '__main__':
-    
-    ncases = load_num()[0]
-
-    for c in range(ncases):
+if __name__ == '__main__':    
+    for c in range(load_num()[0]):
         if is_divisible(*load_case()):
             print('Divisible')
         else:

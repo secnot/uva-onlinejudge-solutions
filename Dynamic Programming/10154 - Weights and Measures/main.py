afddbb4b7c8b1a8b8a0bcf056e30b37535f88427
secnot <secnot@secnot.com>
2017-02-05 16:08:53 +0100

    Solved 10154 - Weights and Measures

--- Dynamic Programming/10154 - Weights and Measures/main.py
+++ Dynamic Programming/10154 - Weights and Measures/main.py
@@ -1,29 +1,23 @@
 import sys
 import operator
 
-
 def readturtles():
     return [tuple(map(int, line.split())) for line in sys.stdin]
 
 def stack_all_the_turtles(turtles):
-    max_weight = max(s for _, s in turtles) # Heaviest stack possible
+    """Find tallest stack using LIS longest increasing subsequence"""
     turtles = sorted(turtles, key=operator.itemgetter(1))
 
-    # tallest stack for given weight
-    stacks = [0 for _ in range(max_weight+1)]
-    stacks[0] = 1
+    lis = [float("inf") for _ in turtles]
+    lis[0], longest_seq = 0, 0
 
-    for turtle_weight, turtle_strength in turtles:
+    for t_weight, t_strength in turtles:
+        for j in range(longest_seq, -1, -1):
+            if t_strength > lis[j]+t_weight and t_weight + lis[j] < lis[j+1]:
+                lis[j+1] = lis[j]+t_weight
+                longest_seq = max(longest_seq, j+1)
 
-        for i in range(turtle_strength-turtle_weight, -1, -1):
-            w, n = i, stacks[i] # Stack weight & number of turtles
-
-            # If the current stack is taller than the one stored for this
-            # weight update it
-            if stacks[w+turtle_weight]<=n:
-                stacks[w+turtle_weight] = n+1
-    
-    return max(stacks)-1
+    return longest_seq
 
 if __name__ == '__main__':
     print(stack_all_the_turtles(readturtles()))

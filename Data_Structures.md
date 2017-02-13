a3d58648ea2e3577feecc86f7a6aa5580b4f1cb4
secnot <secnot@secnot.com>
2017-02-13 04:31:35 +0100

    More information, and problems moved to another dir

---
+++ Data_Structures.md
@@ -0,0 +1,81 @@
+# Data Structures
+
+
+## Linked Lists
+
+- [Wikipedia: Linked list](https://en.wikipedia.org/wiki/Linked_list)
+- [Wikipedia: Doubly liked list](https://en.wikipedia.org/wiki/Doubly_linked_list)
+- [A python implementation of a double linked list](code/llist.py)
+- [Coursera: Singly-Linked List (video)](https://www.coursera.org/learn/data-structures/lecture/kHhgK/singly-linked-lists)
+- [Coursera: Doubly-Linked List (video)](https://www.coursera.org/learn/data-structures/lecture/jpGKD/doubly-linked-lists)
+- [Python: Singly Linked Lists (video)](https://www.youtube.com/watch?v=Ast5sKQXxEU)
+
+
+## Stacks (LIFO)
+
+[What is a Stack (video)](https://www.youtube.com/watch?v=FNZ5o9S9prU)
+[Stacks and Queues](https://en.wikibooks.org/wiki/Data_Structures/Stacks_and_Queues)
+
+
+In python stacks can be easily implemente using lists:
+
+```python
+stack = []
+
+# Push elements into the stack
+stack.append(6)
+stack.append(2)
+stack.append(3)
+
+# Return the value of the last item pushed into the stack
+stack[-1] # returns 3
+
+# Remove and returns the last element pushed onto the stack
+stack.pop() # returns 3
+stack.pop() # returns 2
+stack.pop() # returns 6
+```
+
+# Queues (FIFO)
+
+For Queues python has [deque](https://docs.python.org/2/library/collections.html#collections.deque) 
+from **collections** module:
+
+```python
+from collections import deque
+
+q = deque()
+
+# Equeue item at the end of the queue
+q.append(3)
+q.append(7)
+q.append(6)
+q.append(1)
+
+# dequeue item from the front of the queue
+q.popleft() # returns 3
+q.popleft() # returns 7
+```
+
+## Dictionaries
+[Introduction to Hash Tables (video)](https://www.youtube.com/watch?v=MfhjkfocRR0)
+[MIT 6.006: 8. Hashing with Chaining (video)](https://www.youtube.com/watch?v=0M_kIqhwbFo&list=PLUl4u3cNGP61Oq3tWYp6V_F-5jb5L2iHb&index=8)
+[MIT 6.006: 9. Table Doubling, Karp-Rabin (video)](https://www.youtube.com/watch?v=BRO7mVIFt08&index=9&list=PLUl4u3cNGP61Oq3tWYp6V_F-5jb5L2iHb)
+Dictionaries are built in 
+
+## Priority Queue
+
+
+## Videos
+
+- [ ] [UC Berkeley 61B (Spring 2014): Data Structures (25 videos)](https://www.youtube.com/watch?v=mFPmKGIrQs4&list=PL-XXv-cvA_iAlnI-BQr9hjqADPBtujFJd)
+
+
+
+## Problems
+
+
+[10010 - Where's Whaldorf?]
+[10038 - Jolly Jumpers]
+[843 - Crypt Kicker]
+[850 - Crypt Kicker II]
---
+++ code/llist.py
@@ -0,0 +1,286 @@
+class Node(object):
+
+    __slots__ = ('data', 'nxt', 'prev')
+
+    def __init__(self, data, nxt=None, prev=None):
+        self.data = data
+        self.nxt = nxt
+        self.prev = prev
+
+class LinkedList(object):
+
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._length = 0
+        self._index = None
+
+    @property
+    def first(self):
+        return self[0]
+    
+    @property
+    def last(self):
+        return self[-1]
+
+    def add_last(self, data):
+        """Add element at the end of the list"""
+        node = Node(data, prev=self._tail)
+
+        if not self._head:
+            self._head = node
+            self._tail = node
+        else:
+            self._tail.nxt = node
+            self._tail = node
+
+        self._length += 1
+    
+    def add_first(self, data):
+        """Add element at the start of the list"""
+        node = Node(data, nxt=self._head)
+
+        if not self._head:
+            self._head = node
+            self._tail = node
+        else:
+            self._head.prev = node
+            self._head = node
+
+        self._length += 1
+
+    def remove_last(self):
+        """Remove last element from linked list and return value"""
+        if self._length == 0:
+            raise IndexError("Tried to remove from an empty LinkedList")
+        
+        # Fix iteration index if it is pointing to the last node
+        if self._tail == self._index:
+            self._index = None
+
+        # Remove tail
+        data = self._tail.data
+        self._tail = self._tail.prev
+
+        if self._length == 1:
+            self._head = None
+        else:
+            self._tail.nxt = None
+            
+        self._length -= 1
+        return data
+
+    def remove_first(self):
+        """Remove first element from linked list and return value"""
+        if self._length == 0:
+            raise IndexError("Tried to Remove from an empty LinkedList")
+
+        # Fix iteration index if it is pointing to first node
+        if self._head == self._index:
+            self._index = self._head.nxt
+
+        # Remove head
+        data = self._head.data
+        self._head = self._head.nxt
+
+        if self._length == 1:
+            self._tail = None
+        else:
+            self._head.prev = None
+
+        self._length -= 1
+        return data
+
+    def remove(self, data):
+        """Remove all data ocurrences from the linked list"""
+        if self._length == 0:
+            return
+ 
+        current = self._head
+
+        while current:
+            if current.data != data:
+                current = current.nxt
+                continue
+
+            # Try to recover gracefully from removing current iter index
+            if self._index == current:
+                self._index = current.nxt
+
+            if current.nxt:
+                current.nxt.prev = current.prev
+            if current.prev:
+                current.prev.nxt = current.nxt
+            
+            if self._head == current:
+                self._head = current.nxt
+            if self._tail == current:
+                self._tail = current.prev
+            
+            current = current.nxt
+            self._length -= 1
+
+    def __getitem__(self, key):
+        """return value of the node in 'key' position"""
+        if self._length == 0 or key >= self._length or key < -self._length:
+            raise IndexError("index out of range")
+
+        if key < 0:
+            key = self._length+key
+
+        # Start from head or tail depending of which is closest to key
+        if key//2 <= self._length:
+            pt = self._head
+            for _ in range(key):
+                pt = pt.nxt
+        else:
+            pt = self._tail
+            for _ in range(self._length-key-1):
+                pt = pt.prev
+            
+        return pt.data
+
+    def __iter__(self):
+        self._index = self._head
+        return self
+
+    def __next__(self):
+        if not self._index:
+            raise StopIteration
+
+        data = self._index.data
+        self._index = self._index.nxt
+        return data
+
+    def __contains__(self, data):
+        pt = self._head
+
+        while pt:
+            if pt.data == data:
+                return True
+            pt = pt.nxt
+
+        return False
+
+    def __len__(self):
+        return self._length
+
+
+
+
+if __name__ == '__main__':
+
+    # Some tests
+    l = LinkedList()
+
+    # Adding and indexing data
+    l.add_last(33)
+
+    assert len(l) == 1
+    assert l[0] == 33
+    assert l[-1] == 33
+
+    l.add_last(55)
+    l.add_first(22)
+
+    assert len(l) == 3
+    assert l.first == 22
+    assert l.last == 55
+    assert l[0] == 22
+    assert l[1] == 33
+    assert l[2] == 55
+    assert l[-1] == 55
+    assert l[-2] == 33
+    assert l[-3] == 22
+
+    # Test removing data
+    assert l.remove_first() ==22
+
+    assert len(l) == 2
+    assert l[0] == 33
+    assert l[1] == 55
+    assert l[-1] == 55
+    assert l[-2] == 33
+
+    assert l.remove_last() == 55
+    assert len(l) == 1
+    assert l[0] == 33
+    assert l[-1] == 33
+
+    assert l.remove_first() == 33
+    assert len(l) == 0
+
+    # Test contains
+    l.add_first(444)
+    assert len(l) == 1
+    assert l[0] == 444
+    assert l[-1] == 444
+
+    assert 444 in l
+    assert 33 not in l
+
+    l.add_last(33)
+    assert 444 in l
+    assert 33 in l
+
+    l.remove_last()
+    l.remove_last()
+    assert 444 not in l
+    assert 33 not in l
+
+    # Test remove
+    l.add_last(12)
+    l.add_last(33)
+    l.add_last(12)
+    l.add_last(44)
+    l.add_last(12)
+    
+    assert len(l) == 5
+    
+    l.remove(12)
+    assert len(l) == 2
+    assert l[0] == 33
+    assert l[1] == 44
+
+    l.remove(44)
+    assert len(l) == 1
+    assert l[-1] == 33
+
+    l.remove(33)
+    assert len(l) == 0
+
+    # Test iteration
+    l.add_last(1)
+    l.add_last(3)
+    l.add_last(5)
+    assert list(iter(l)) == [1, 3, 5]
+
+    # Remove while iterating
+    iter(l)
+    assert next(l) == 1
+    l.remove(3) 
+    assert next(l) == 5
+    assert list(iter(l)) == [1, 5]
+
+    # Remove first element iterations
+    l = LinkedList()
+    l.add_last(1)
+    l.add_last(3)
+    l.add_last(5)
+    
+    iter(l)
+    l.remove_first()
+    assert next(l) == 3
+    assert next(l) == 5
+
+    # Remove last element while iterating
+    l = LinkedList()
+    l.add_last(1)
+    l.add_last(3)
+    l.add_last(5)
+    
+    iter(l)
+    assert next(l) == 1
+    assert next(l) == 3
+    l.remove_last()
+    assert list(l) == [1, 3]
